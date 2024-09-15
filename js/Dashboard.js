f46fb10280ba5c035e0f71a00fa198b86801b7b2
Daniel <daniel.carrascomen@gmail.com>
2024-09-15 09:27:36 -0600

    a

--- js/Dashboard.js
+++ js/Dashboard.js
@@ -4,19 +4,31 @@ const chart = document.querySelector('#chart').getContext('2d');
 new Chart(chart, {
     type: 'line',
     data: {
-        labels: ['Jan', 'Feb', 'Mart', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec', 'Jan', 'Feb', 'Mart', 'Apr'],
+        labels: ['10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm'],
 
         datasets: [
             {
-                label: 'GOLD',
+                label: '1D',
                 data: [1796, 1908, 1936, 1895, 1836, 1807, 1765, 1710, 1660, 1632, 1768, 1822, 1928, 1881],
-                borderColor: 'green',
+                borderColor: 'purple',
                 borderWidth: 2
             },
             {
-                label: 'ETH',
+                label: '5D',
                 data: [2688, 2922, 3282, 2728, 1940, 1071, 1679, 1554, 1328, 1572, 1294, 1194, 1585, 1677],
-                borderColor: 'blue',
+                borderColor: 'grey',
+                borderWidth: 2
+            },
+            {
+                label: '1M',
+                data: [1796, 1908, 1936, 1895, 1836, 1807, 1765, 1710, 1660, 1632, 1768, 1822, 1928, 1881],
+                borderColor: 'grey',
+                borderWidth: 2
+            },
+            {
+                label: 'BSE',
+                data: [2688, 2922, 3282, 2728, 1940, 1071, 1679, 1554, 1328, 1572, 1294, 1194, 1585, 1677],
+                borderColor: 'grey',
                 borderWidth: 2
             }
         ]
